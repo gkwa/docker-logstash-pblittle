31690a1278da4573a2181cef683215303184ed8e
Taylor Monacelli <taylor.monacelli@streambox.com>
2015-09-13 20:50:16 -0700

    t

---
+++ 1.4/README.md
@@ -0,0 +1,3 @@
+```
+vagrant ssh -c 'cd /vagrant && sudo sh -x test.sh'
+```
--- 1.4/test.sh
+++ 1.4/test.sh
@@ -35,5 +35,5 @@ taylor/logstash:latest
 
 wget -nc ftp://ita.ee.lbl.gov/traces/NASA_access_log_Jul95.gz
 [ ! -f access_log_Jul95 ] && 7za x NASA_access_log_Jul95.gz
-sleep 60 # wait for server to come online
+sleep 30 # wait for server to come online
 cat access_log_Jul95 | nc -v -w 1 127.0.0.1 1514
